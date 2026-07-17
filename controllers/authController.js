a6ad06c5e86bba2608defb35a014a19293ac4eb4
tomisanti05 <thomassantiago005@gmail.com>
2026-07-16 23:42:54 -0300

    verificacion de login en auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,11 @@
 const newUser = await User.create({nombre ,email, password })
+
+const user = await User.findone({where:{email} })
+if (!user){
+    return res.status(404).json({error: 'usuario no encontrado'})
+}
+
+const isValid = await user.validarPassword(password)
+if (!isValid){
+    return res.status(401).json({error: 'Contraseña incorrecta'});
+}
