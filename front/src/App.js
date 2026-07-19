301817be37c5a456225841ad2b20373f9cee91fe
AriadnaLavizzar <lavizzariariadna@gmail.com>
2026-07-19 00:22:20 -0300

    Corregi un error

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,7 @@ import { Layout } from "./components/Layout";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from './pages/Login';
 import Register from "./pages/Register";
-import './style/global.css';
+import './styles/global.css';
 
 function App() {
     return (
